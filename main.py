dca2d758e88dc888152a0ed0f321ee1a56fef5e6
15900568361 <1975853010@qq.com>
2025-04-30 21:43:46 +0800

    1

--- main.py
+++ main.py
@@ -852,7 +852,7 @@ class MyPlugin(Star):
         logger.info(result)
         content = ""
         for item in result:
-            content += f"{item["name"]}\n颜色：{item['color']}\n价格：{item['price']}\n分线：{item['line']}"
+            content += f"{item['name']}\n颜色：{item['color']}\n价格：{item['price']}\n分线：{item['line']}"
         yield event.plain_result(content.strip())
 
     async def terminate(self):
