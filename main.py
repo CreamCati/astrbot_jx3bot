9302d7d70be82f6750a2c3e92e83b1fb55b5997d
15900568361 <1975853010@qq.com>
2025-04-30 22:42:19 +0800

    1

--- main.py
+++ main.py
@@ -855,7 +855,7 @@ class MyPlugin(Star):
     @filter.command("家园鲜花")
     async def active_list_calendar(self, event: AstrMessageEvent, name: str, map: str, server="唯我独尊"):
         """此接口用于查询家园系统中特定鲜花的最高价格及其对应的采集线路，包括区服和地图信息。"""  # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
-
+        logger.info(f"server{server}")
         if name in flowers:
             if map in homes:
                 result = api.home_flower(server=server, name=name, map=map)[map]
