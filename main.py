10e30799346db99a9e64eb15a2ce5370fea3c4ba
15900568361 <1975853010@qq.com>
2025-04-30 21:42:11 +0800

    1

--- main.py
+++ main.py
@@ -847,11 +847,13 @@ class MyPlugin(Star):
     @filter.command("家园鲜花")
     async def active_list_calendar(self, event: AstrMessageEvent, name: str, map: str, server: str):
         """此接口用于查询家园系统中特定鲜花的最高价格及其对应的采集线路，包括区服和地图信息。"""  # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+        logger.info(f"{server}{name}{map}")
         result = api.home_flower(server=server or self.server, name=name, map=map)[map]
-
+        logger.info(result)
+        content = ""
         for item in result:
-            logger.info(item)
-        yield event.plain_result("")
+            content += f"{item["name"]}\n颜色：{item['color']}\n价格：{item['price']}\n分线：{item['line']}"
+        yield event.plain_result(content.strip())
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
